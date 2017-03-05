93e438db3ccf064fca81818ef6d041a5769a7084
Jorge S <jorge.solis.es@gmail.com>
2017-03-05 18:20:01 +0100

    Commit prueba Unity MoonFlower

---
+++ Moonflowers/Assets/clickCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class clickCube : MonoBehaviour {
+
+    public Material ColorMaterial;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+       ColorMaterial = GetComponent<Renderer>().material; // Carga el material.
+       rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        ColorMaterial.color = Color.green; // Cambia color a verde.
+        rb.useGravity = true;              // Activa gravedad.
+    }
+}
